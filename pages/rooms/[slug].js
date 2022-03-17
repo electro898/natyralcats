306b78314d42c595a0325a72ea10fe1e8a524b60
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-17 23:13:45 +0100

    Update [slug].js

--- pages/rooms/[slug].js
+++ pages/rooms/[slug].js
@@ -1,5 +1,5 @@
 import Layout from "../../components/Layout";
-import PageWrapper from "../components/PageWrapper";
+import PageWrapper from "../../components/PageWrapper";
 
 function RoomItem(props) {
   // console.log(props);
